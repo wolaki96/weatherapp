73d9ce7b88ca4c953b581535e6734033a53c5650
Squiddy <wolaki96@gmail.com>
2022-10-13 22:42:08 -0400

    added some code

--- weather.js
+++ weather.js
@@ -1,25 +1,36 @@
 // my api key for open weather 4b10e0d30d2d231648ee51e44ca7d322
 
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={4b10e0d30d2d231648ee51e44ca7d322}
+
 var appid = "4b10e0d30d2d231648ee51e44ca7d322";
-var input = "orlando";
+var input = "Tokyo";
 var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=" + appid;
-
-
 var cityBtn = document.getElementById("search");
 
-
-
-fetch(queryURL)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
    
 function weatherSearch(){
         cityBtn.addEventListener('click', weatherAsk);
     } 
+
+fetch(queryURL, {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     } ,
+//     body: JSON.stringify({
+        
+//     })
+}).then((response) => response.json())
+    .then((data) => console.log(data));
+    var city = document.getElementById("city")
+    
+    
+
+
  function weatherAsk(){
-        loadJSON(queryURL, data);
+   
+    loadJSON(queryURL, data);
         input = select("#citySearch")
+        
     }
 
 
